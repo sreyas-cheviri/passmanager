24582a180d5e91196d8d6eda4ff70501acaa2b99
sreyascheviri <sreyascheviri@gmail.com>
2024-10-31 18:41:03 +0530

    dddf

--- public/script.js
+++ public/script.js
@@ -1,12 +1,12 @@
 const form = document.getElementById('password-form');
 const passli = document.getElementById('password-list');
-// const dropdown = document.getElementById('dropdown');
+
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
     const name = document.getElementById('name').value;
     const password = document.getElementById('password').value;
     try {
-        const response = await axios.post('/add', { name, password });
+        const response = await axios.post('/add', { name, password },{headers:{"authorization":"testing"}});
         const data = response.data;
         localStorage.setItem('data', JSON.stringify(data));
         displayp(data); form.reset();
@@ -21,14 +21,11 @@ function displayp(data) {
     data.forEach(({ name, password }) => {
         const list = document.createElement('li');
         list.innerHTML = `${name}:${password}`;
-
         list.addEventListener('click', () => {
             list.classList.toggle('strike');
             deletepas(name);
         })
-
         passli.appendChild(list);
-
     })
 }
 
@@ -40,18 +37,15 @@ function dropdown() {
     }
 }
 
-
-
 async function deletepas(name) {
     try {
         const val = await axios.post('/delete', { name })
         localStorage.setItem('data', JSON.stringify(val.data));
-
     } catch (error) {
         console.log(error);
-
     }
 }
+
 document.addEventListener('DOMContentLoaded', () => {
     const storedData = JSON.parse(localStorage.getItem('data')) || [];
     displayp(storedData);
